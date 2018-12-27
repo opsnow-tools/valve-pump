5ec9edb49e21b9dfbe2b70b585d97bb96d54fbde
nalbam <me@nalbam.com>
2018-12-27 19:21:45 +0900

    back quote

--- src/com/opsnow/valve/Butler.groovy
+++ src/com/opsnow/valve/Butler.groovy
@@ -438,25 +438,25 @@ def mvn_sonar() {
 }
 
 def failure(token = "", type = "", name = "") {
-    slack("$token", "danger", "$type Failure", "\`$name\`", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER>")
+    slack("$token", "danger", "$type Failure", "\\\`$name\\\`", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER>")
 }
 
 def success(token = "", type = "", name = "", version = "", namespace = "", base_domain = "", cluster = "") {
     if (cluster) {
         def link = "https://$name-$namespace.$base_domain"
-        slack("$token", "good", "$type Success", "\`$name\` \`$version\` :satellite: \`$namespace\` :earth_asia: \`$cluster\`", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER> : <$link|$name-$namespace>")
+        slack("$token", "good", "$type Success", "\\\`$name\\\` \\\`$version\\\` :satellite: \\\`$namespace\\\` :earth_asia: \\\`$cluster\\\`", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER> : <$link|$name-$namespace>")
     } else if (base_domain) {
     def link = "https://$name-$namespace.$base_domain"
-        slack("$token", "good", "$type Success", "\`$name\` \`$version\` :satellite: \`$namespace\`", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER> : <$link|$name-$namespace>")
+        slack("$token", "good", "$type Success", "\\\`$name\\\` \\\`$version\\\` :satellite: \\\`$namespace\\\`", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER> : <$link|$name-$namespace>")
     } else if (namespace) {
-        slack("$token", "good", "$type Success", "\`$name\` \`$version\` :rocket: \`$namespace\`", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER>")
+        slack("$token", "good", "$type Success", "\\\`$name\\\` \\\`$version\\\` :rocket: \\\`$namespace\\\`", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER>")
     } else {
-        slack("$token", "good", "$type Success", "\`$name\` \`$version\` :heavy_check_mark:", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER>")
+        slack("$token", "good", "$type Success", "\\\`$name\\\` \\\`$version\\\` :heavy_check_mark:", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER>")
     }
 }
 
 def proceed(token = "", type = "", name = "", version = "", namespace = "") {
-    slack("$token", "warning", "$type Proceed?", "\`$name\` \`$version\` :rocket: \`$namespace\`", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER>")
+    slack("$token", "warning", "$type Proceed?", "\\\`$name\\\` \\\`$version\\\` :rocket: \\\`$namespace\\\`", "$JOB_NAME <$RUN_DISPLAY_URL|#$BUILD_NUMBER>")
 }
 
 def slack(token = "", color = "", title = "", message = "", footer = "") {
